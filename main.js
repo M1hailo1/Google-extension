db1dbcfa9ca814744259fb756b01257c6ce19c5e
Mihailo Maricic <mihailo.maricic213@gmail.com>
2026-04-26 23:00:25 +0200

    Add chrome tab pickup functionality & delete all button, add localStorage

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
 const inputElement = document.querySelector("#input-el");
 const inputButton = document.querySelector("#input-btn");
 const outputElement = document.querySelector("#output-ul");
+const deleteButton = document.querySelector("#delete-btn");
+const saveButton = document.querySelector("#save-btn");
+
+const localStorageLeads = JSON.parse(localStorage.getItem("leads"));
+
 let myLeads = [];
+
+if (localStorageLeads) {
+  myLeads = localStorageLeads;
+  render(myLeads);
+}
+
+saveButton.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("leads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 inputButton.addEventListener("click", () => {
   if (inputElement.value != "") {
     myLeads.push(inputElement.value);
-    outputElement.innerHTML += `
-      <li>
-        <a href='${inputElement.value}' 
-          target='_blank'>${inputElement.value}
-        </a>
-      </li>`;
+    localStorage.setItem("leads", JSON.stringify(myLeads));
+    render(myLeads);
     inputElement.value = "";
   }
 });
 
-// function renderLeads() {
-//   let listItems = "";
-//   for (let i = 0; i < myLeads.length; i++) {
-//     listItems += "<li>" + myLeads[i] + "</li>";
-//   }
+function render(array) {
+  let listItems = "";
+  for (let i = 0; i < array.length; i++) {
+    listItems += `
+      <li>
+        <a href='${array[i]}' 
+          target='_blank'>${array[i]}
+        </a>
+      </li>`;
+  }
 
-//   outputElement.innerHTML = listItems;
-// }
+  outputElement.innerHTML = listItems;
+}
+
+deleteButton.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
